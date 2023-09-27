0d9a594401b167df12380d1e378bd37029011ef7
Dale Fabic <130316665+aquinofabic@users.noreply.github.com>
2023-09-27 13:32:04 +0100

    Init

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] usernameArray = {"alex", "emma"};
+        String[] passwordArray = {"sunshine", "haskell"};
+
+        System.out.println("Enter username:");
+        String usernameInput = scanner.nextLine();
+        System.out.println("Enter password:");
+        String passwordInput = scanner.nextLine();
+
+        boolean isValid = false;
+
+        for (int i = 0; i < usernameArray.length; i++ ) {
+            if (usernameInput.equals(usernameArray[i]) && passwordInput.equals(passwordArray[i])) {
+                isValid = true;
+            }
+        }
+        if (isValid) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
+    }
+}
